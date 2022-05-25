8d79a6e8a3a8fb142c9f9cdcf362059194766ab7
EduardoReyes <eduardo.mreyes94@gmail.com>
2022-05-25 16:48:45 -0500

    Primer commit de los ejercicios de Javascript

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// 1. Solicitar 3 números (entre el 1 y el 100)  y definir cual es el mayor
+
+// SOLICITUD AL USUARIO
+// alert ("Elige tres números entre el 1 y el 100!");
+// num1 = parseFloat(prompt("Ingresa el primer número: "));
+// num2 = parseFloat(prompt("Ingresa el segundo número: "));
+// num3 = parseFloat(prompt("Ingresa el tercer número: "));
+// let mayor;
+
+// //CONDICION
+// if(num1>=num2 && num1>=num3 ) {
+//        mayor = num1;
+       
+//     } else if(num2>=num1 && num2>=num3) {
+//      mayor = num2;
+//     }
+//     else {
+//         mayor = num3;
+//     }
+
+// //MUESTRA EL RESULTADO EN CONSOLA 
+// //PODRÍAS PONER ALERT PERO WAKALA
+// console.log("El número mayor es: " + mayor);
+
+// // 2. Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
+// alert ("Elige tres números entre el 1 y el 100!");
+// num1 = parseFloat(prompt("Ingresa el primer número: "));
+// num2 = parseFloat(prompt("Ingresa el segundo número: "));
+// num3 = parseFloat(prompt("Ingresa el tercer número: "));
+// let menor;
+// if(num1<=num2 && num1<=num3 ) {
+//      menor = num1;  
+//} else if(num2<=num1 && num2<=num3) {
+//      menor = num2;
+//}
+//  else {
+//      menor = num3;
+//}
+// console.log("El número menor es: " + menor);
+
+// 4. Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+// alert ("Elige cualquier número entre el 100 y el 200");
+// let entero = (prompt("Ingresa tu número: " ));
+
+// if (entero % 3 == 0){
+//     console.log("Si es múltiplo de 3 :D");
+// }   else{
+//     console.log("No es múltiplo de 3 :(");
+// }
+
+// 5. Elaborar un algoritmo para leer 3 números y determinar sí uno es la suma de los otros dos
+// alert ("Elige tres números!");
+// num1 = parseFloat(prompt("Ingresa el primer número: "));
+// num2 = parseFloat(prompt("Ingresa el segundo número: "));
+// num3 = parseFloat(prompt("Ingresa el tercer número: "));
+
+// if (num1==(num2 + num3)){
+//     console.log("El número 1 es igual a la sumatoria del número 2 y el 3!");
+// } 
+// else if (num2 == (num1 + num3)){
+//     console.log("El número 2 es es igual a la sumatoria del 1 y el 3!");
+// }  
+// else if (num3 == (num1 + num2)){
+//     console.log("El número 3 es igual a la sumatoria del 1 y el 2!");
+// }
+// else{
+//     console.log("Ninguno número son sumatoria de los otros dos");
+// }
+
+// 6. Elabora un algoritmo para leer un número y determinar si es par o impar
+// let par = (prompt("Ingresa un número: "));
+// if (par % 2 == 0){
+//     console.log("SIES PAR");
+// }
+//     else {
+//     console.log();("NOES PAR");
+// }
